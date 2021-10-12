7e560311769fc95d178bc478acdc3eb10c62da6d
luuyiran <luuyiran@gmail.com>
2021-10-12 17:25:51 +0800

    v0.2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <stdlib.h>         // qsort
+#include <time.h>           // clock_t
 #include <string.h>         // memcpy
 #include <stdbool.h>        // bool 
 #include <math.h>           // sqrt 
-#include <wchar.h>
+#include <wchar.h>          // wchar_t
 #include <locale.h>         // fix printf wchar_t
 #include <easyx.h>          // ui
 
@@ -39,8 +41,13 @@
 #define PIECE_CANNON      5    // 炮
 #define PIECE_PAWN        6    // 兵
 
-// 最大的生成走法数
-#define MAX_GEN_MOVES  128 
+
+#define MAX_GEN_MOVES   128                 // 最大的生成走法数
+#define LIMIT_DEPTH     32                  // 最大的搜索深度
+#define MATE_VALUE      10000               // 最高分值，即将死的分值
+#define WIN_VALUE       (MATE_VALUE - 100)  // 搜索出胜负的分值界限，超出此值就说明已经搜索出杀棋了
+#define ADVANCED_VALUE  3                   // 先行权分值
+
 
 // 判断棋子是否在棋盘中的数组
 const char inBoard[256] = {
@@ -207,14 +214,130 @@ const wchar_t* lables[32] = {
 
 #define LABLE(id)  lables[pos.curboard[id]]
 
+// 子力位置价值表
+const int cucvlPiecePos[7][256] = {
+  { // 帅(将)
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  2,  2,  2,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0, 11, 15, 11,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+  }, { // 仕(士)
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0, 20,  0, 20,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0, 23,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0, 20,  0, 20,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+  }, { // 相(象)
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0, 20,  0,  0,  0, 20,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0, 18,  0,  0,  0, 23,  0,  0,  0, 18,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0, 20,  0,  0,  0, 20,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+  }, { // 马
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0, 90, 90, 90, 96, 90, 96, 90, 90, 90,  0,  0,  0,  0,
+    0,  0,  0, 90, 96,103, 97, 94, 97,103, 96, 90,  0,  0,  0,  0,
+    0,  0,  0, 92, 98, 99,103, 99,103, 99, 98, 92,  0,  0,  0,  0,
+    0,  0,  0, 93,108,100,107,100,107,100,108, 93,  0,  0,  0,  0,
+    0,  0,  0, 90,100, 99,103,104,103, 99,100, 90,  0,  0,  0,  0,
+    0,  0,  0, 90, 98,101,102,103,102,101, 98, 90,  0,  0,  0,  0,
+    0,  0,  0, 92, 94, 98, 95, 98, 95, 98, 94, 92,  0,  0,  0,  0,
+    0,  0,  0, 93, 92, 94, 95, 92, 95, 94, 92, 93,  0,  0,  0,  0,
+    0,  0,  0, 85, 90, 92, 93, 78, 93, 92, 90, 85,  0,  0,  0,  0,
+    0,  0,  0, 88, 85, 90, 88, 90, 88, 90, 85, 88,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+  }, { // 车
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,206,208,207,213,214,213,207,208,206,  0,  0,  0,  0,
+    0,  0,  0,206,212,209,216,233,216,209,212,206,  0,  0,  0,  0,
+    0,  0,  0,206,208,207,214,216,214,207,208,206,  0,  0,  0,  0,
+    0,  0,  0,206,213,213,216,216,216,213,213,206,  0,  0,  0,  0,
+    0,  0,  0,208,211,211,214,215,214,211,211,208,  0,  0,  0,  0,
+    0,  0,  0,208,212,212,214,215,214,212,212,208,  0,  0,  0,  0,
+    0,  0,  0,204,209,204,212,214,212,204,209,204,  0,  0,  0,  0,
+    0,  0,  0,198,208,204,212,212,212,204,208,198,  0,  0,  0,  0,
+    0,  0,  0,200,208,206,212,200,212,206,208,200,  0,  0,  0,  0,
+    0,  0,  0,194,206,204,212,200,212,204,206,194,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+  }, { // 炮
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,100,100, 96, 91, 90, 91, 96,100,100,  0,  0,  0,  0,
+    0,  0,  0, 98, 98, 96, 92, 89, 92, 96, 98, 98,  0,  0,  0,  0,
+    0,  0,  0, 97, 97, 96, 91, 92, 91, 96, 97, 97,  0,  0,  0,  0,
+    0,  0,  0, 96, 99, 99, 98,100, 98, 99, 99, 96,  0,  0,  0,  0,
+    0,  0,  0, 96, 96, 96, 96,100, 96, 96, 96, 96,  0,  0,  0,  0,
+    0,  0,  0, 95, 96, 99, 96,100, 96, 99, 96, 95,  0,  0,  0,  0,
+    0,  0,  0, 96, 96, 96, 96, 96, 96, 96, 96, 96,  0,  0,  0,  0,
+    0,  0,  0, 97, 96,100, 99,101, 99,100, 96, 97,  0,  0,  0,  0,
+    0,  0,  0, 96, 97, 98, 98, 98, 98, 98, 97, 96,  0,  0,  0,  0,
+    0,  0,  0, 96, 96, 97, 99, 99, 99, 97, 96, 96,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+  }, { // 兵(卒)
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  9,  9,  9, 11, 13, 11,  9,  9,  9,  0,  0,  0,  0,
+    0,  0,  0, 19, 24, 34, 42, 44, 42, 34, 24, 19,  0,  0,  0,  0,
+    0,  0,  0, 19, 24, 32, 37, 37, 37, 32, 24, 19,  0,  0,  0,  0,
+    0,  0,  0, 19, 23, 27, 29, 30, 29, 27, 23, 19,  0,  0,  0,  0,
+    0,  0,  0, 14, 18, 20, 27, 29, 27, 20, 18, 14,  0,  0,  0,  0,
+    0,  0,  0,  7,  0, 13,  0, 16,  0, 13,  0,  7,  0,  0,  0,  0,
+    0,  0,  0,  7,  0,  7,  0, 15,  0,  7,  0,  7,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+  }
+};
 
-// 局面结构
-typedef struct positionStruct {
-    bool blackPlayer;           // 轮到谁走，0=红方，1=黑方
-    char curboard[256];  // 棋盘上的棋子
-} positionStruct;
-
-positionStruct pos;  // 局面实例
 
 #define _XY(i)   (BOARD_EDGE + i * PIECE_SIZE)
 // 格子中心的 x 坐标，像素坐标，用于绘制棋盘和棋子
@@ -278,6 +401,9 @@ inline int COL(int id) { return id & 0XF; }
 // 根据纵坐和横坐标获得格子
 inline int COORD_XY(int x, int y) { return x + (y << 4); }
 
+// 翻转格子
+inline int SQUARE_FLIP(int sq) { return 254 - sq; }
+
 // 纵坐标水平镜像
 inline int FILE_FLIP(int x) { return 14 - x; }
 
@@ -316,24 +442,24 @@ inline int KNIGHT_PIN(int idSrc, int idDst) {
 }
 
 // 是否未过河
-inline bool HOME_HALF(int id, int isBlack) { return (id & 0x80) != (isBlack << 7); }
+inline bool HOME_HALF(int id, int isBlack) { return (id & 0X80) != (isBlack << 7); }
 
 // 是否已过河
-inline bool AWAY_HALF(int id, int isBlack) { return (id & 0x80) == (isBlack << 7); }
+inline bool AWAY_HALF(int id, int isBlack) { return (id & 0X80) == (isBlack << 7); }
 
 // 是否在河的同一边 黑方 ID - 51 - 127, 红方 128 - 255  0X80 = 128
 inline bool SAME_HALF(int idSrc, int idDst) {
-    return ((idSrc ^ idDst) & 0x80) == 0;
+    return ((idSrc ^ idDst) & 0X80) == 0;
 }
 
 // 是否在同一行
 inline bool SAME_RANK(int idSrc, int idDst) {
-    return ((idSrc ^ idDst) & 0xf0) == 0;
+    return ((idSrc ^ idDst) & 0XF0) == 0;
 }
 
 // 是否在同一列
 inline bool SAME_FILE(int idSrc, int idDst) {
-    return ((idSrc ^ idDst) & 0x0f) == 0;
+    return ((idSrc ^ idDst) & 0X0F) == 0;
 }
 
 // 获得红黑标记(红子是8，黑子是16)
@@ -343,7 +469,7 @@ inline int SIDE_TAG(int isBlack) { return 8 + (isBlack << 3); }
 inline int OPP_SIDE_TAG(int isBlack) { return 16 - (isBlack << 3); }
 
 // 获得走法的起点
-inline int SRC(int mv) { return mv & 255; }
+inline int SRC(int mv) { return mv & 0XFF; }
 
 // 获得走法的终点
 inline int DST(int mv) { return mv >> 8; }
@@ -356,14 +482,39 @@ inline int MIRROR_MOVE(int mv) {
     return MOVE(MIRROR_SQUARE(SRC(mv)), MIRROR_SQUARE(DST(mv)));
 }
 
+// 局面结构
+typedef struct positionStruct {
+    bool blackPlayer;           // 轮到谁走，0=红方，1=黑方
+    int  vlRed, vlBlack;        // 红、黑双方的子力价值
+    int  nDistance;             // 距离根节点的步数
+    char curboard[256];         // 棋盘上的棋子
+} positionStruct;
+
+positionStruct pos;  // 局面实例
+
 void changeSide(positionStruct* pos) {  // 交换走子方
     pos->blackPlayer ^= 1;
 }
 void addPiece(positionStruct* pos, int id, int type) {  // 在棋盘上放一枚棋子
     pos->curboard[id] = type;
+    // 红方加分，黑方(注意"cucvlPiecePos"取值要颠倒)减分
+    if (type < 16)
+      pos->vlRed += cucvlPiecePos[type - 8][id];
+    else
+      pos->vlBlack += cucvlPiecePos[type - 16][SQUARE_FLIP(id)];
 }
-void delPiece(positionStruct* pos, int id) {  // 从棋盘上拿走一枚棋子
+void delPiece(positionStruct* pos, int id, int type) {  // 从棋盘上拿走一枚棋子
     pos->curboard[id] = 0;
+    if (type < 16)
+      pos->vlRed -= cucvlPiecePos[type - 8][id];
+    else
+      pos->vlBlack -= cucvlPiecePos[type - 16][SQUARE_FLIP(id)];
+}
+
+// 局面评价函数
+int evaluate(positionStruct* pos) {
+    int valueBlack = pos->vlBlack - pos->vlRed;
+    return (pos->blackPlayer ? valueBlack : -valueBlack) + ADVANCED_VALUE;
 }
 
 // 搬一步棋的棋子
@@ -372,9 +523,10 @@ int movePiece(positionStruct* pos, int mv) {
     idSrc = SRC(mv);
     idDst = DST(mv);
     pcCaptured = pos->curboard[idDst];
-    delPiece(pos, idDst);
+    if (pcCaptured)
+        delPiece(pos, idDst, pcCaptured);
     type = pos->curboard[idSrc];
-    delPiece(pos, idSrc);
+    delPiece(pos, idSrc, type);
     addPiece(pos, idDst, type);
     return pcCaptured;
 }
@@ -385,9 +537,17 @@ void undoMovePiece(positionStruct* pos, int mv, int typeDst) {
     idSrc = SRC(mv);
     idDst = DST(mv);
     typeSrc = pos->curboard[idDst];
-    delPiece(pos, idDst);
+    delPiece(pos, idDst, typeSrc);
     addPiece(pos, idSrc, typeSrc);
-    addPiece(pos, idDst, typeDst);
+    if (typeDst)
+        addPiece(pos, idDst, typeDst);
+}
+
+  // 撤消走一步棋
+void undoMakeMove(positionStruct* pos, int mv, int pcCaptured) {
+    pos->nDistance--;
+    changeSide(pos);
+    undoMovePiece(pos, mv, pcCaptured);
 }
 
 // 判断是否被将军
@@ -464,16 +624,17 @@ bool checked(positionStruct* pos) {
 void renderMove(positionStruct *pos, int mv, int typeDst);
 
 // 走一步棋
-bool makeMove(positionStruct* pos, int mv) {
-    int typeDst;
-    typeDst = movePiece(pos, mv);
+bool makeMove(positionStruct* pos, int mv, int *typeDst, bool showPath) {
+    *typeDst = movePiece(pos, mv);
     if (checked(pos)) {
-        undoMovePiece(pos, mv, typeDst);
+        undoMovePiece(pos, mv, *typeDst);
         return false;
     }
-    // 渲染移动过程
-    renderMove(pos, mv, typeDst);
+    // 是否渲染移动过程
+    if (showPath)
+        renderMove(pos, mv, *typeDst);
     changeSide(pos);
+    pos->nDistance++;
     return true;
 }
 
@@ -734,15 +895,127 @@ bool isMate(positionStruct* pos) {
     return true;
 }
 
-void startup(positionStruct* pos) {  // 初始化棋盘
-    pos->blackPlayer = false;
-    memcpy(pos->curboard, boardStartup, 256);
+// 与搜索有关的全局变量
+struct {
+    int mvResult;              // 电脑走的棋
+    int nHistoryTable[65536];  // 历史表
+} Search;
+
+// "qsort"按历史表排序的比较函数
+int compareHistory(const void* lpmv1, const void* lpmv2) {
+    return Search.nHistoryTable[*(int*)lpmv2] -
+           Search.nHistoryTable[*(int*)lpmv1];
+}
+
+// 超出边界(Fail-Soft)的Alpha-Beta搜索过程
+int searchFull(int vlAlpha, int vlBeta, int nDepth) {
+    int i, nGenMoves, pcCaptured;
+    int vl, vlBest, mvBest;
+    int mvs[MAX_GEN_MOVES];
+    // 一个Alpha-Beta完全搜索分为以下几个阶段
+
+    // 1. 到达水平线，则返回局面评价值
+    if (nDepth == 0) {
+        return evaluate(&pos);
+    }
+
+    // 2. 初始化最佳值和最佳走法
+    vlBest = -MATE_VALUE;  // 这样可以知道，是否一个走法都没走过(杀棋)
+    mvBest = 0;  // 这样可以知道，是否搜索到了Beta走法或PV走法，以便保存到历史表
+
+    // 3. 生成全部走法，并根据历史表排序
+    nGenMoves = generateMoves(&pos, mvs);
+    qsort(mvs, nGenMoves, sizeof(int), compareHistory);
+
+    // 4. 逐一走这些走法，并进行递归
+    for (i = 0; i < nGenMoves; i++) {
+        if (makeMove(&pos, mvs[i], &pcCaptured, false)) {
+            vl = -searchFull(-vlBeta, -vlAlpha, nDepth - 1);
+            undoMakeMove(&pos, mvs[i], pcCaptured);
+
+            // 5. 进行Alpha-Beta大小判断和截断
+            if (vl > vlBest) {  // 找到最佳值(但不能确定是Alpha、PV还是Beta走法)
+                vlBest = vl;  // "vlBest"就是目前要返回的最佳值，可能超出Alpha-Beta边界
+                if (vl >= vlBeta) {   // 找到一个Beta走法
+                    mvBest = mvs[i];  // Beta走法要保存到历史表
+                    break;            // Beta截断
+                }
+                if (vl > vlAlpha) {   // 找到一个PV走法
+                    mvBest = mvs[i];  // PV走法要保存到历史表
+                    vlAlpha = vl;     // 缩小Alpha-Beta边界
+                }
+            }
+        }
+    }
+
+    // 5. 所有走法都搜索完了，把最佳走法(不能是Alpha走法)保存到历史表，返回最佳值
+    if (vlBest == -MATE_VALUE) {
+        // 如果是杀棋，就根据杀棋步数给出评价
+        return pos.nDistance - MATE_VALUE;
+    }
+    if (mvBest != 0) {
+        // 如果不是Alpha走法，就将最佳走法保存到历史表
+        Search.nHistoryTable[mvBest] += nDepth * nDepth;
+        if (pos.nDistance == 0) {
+            // 搜索根节点时，总是有一个最佳走法(因为全窗口搜索不会超出边界)，将这个走法保存下来
+            Search.mvResult = mvBest;
+        }
+    }
+    return vlBest;
+}
+
+// 迭代加深搜索过程
+void searchMain(void) {
+    int i, t, vl;
+
+    // 初始化
+    memset(Search.nHistoryTable, 0, 65536 * sizeof(int));  // 清空历史表
+    t = clock();                                           // 初始化定时器
+    pos.nDistance = 0;                                     // 初始步数
+
+    // 迭代加深过程
+    for (i = 1; i <= LIMIT_DEPTH; i++) {
+        vl = searchFull(-MATE_VALUE, MATE_VALUE, i);
+        // 搜索到杀棋，就终止搜索
+        if (vl > WIN_VALUE || vl < -WIN_VALUE) {
+            break;
+        }
+        // 超过一秒，就终止搜索
+        if (clock() - t > CLOCKS_PER_SEC) {
+            LOG("timeout, searching stoped!\n");
+            break;
+        }
+    }
+    LOG("search depth: %d\n", i);
 }
 
 /********************************************** 图形界面、鼠标输入 *******************************************************/
 double moveX = 0;
 double moveY = 0;
 int idSelected = 0;
+
+// 电脑回应一步棋
+void responseMove(void) {
+    int pcCaptured;
+
+    searchMain();
+    idSelected = SRC(Search.mvResult);
+    makeMove(&pos, Search.mvResult, &pcCaptured, true);
+
+    idSelected = 0;
+    // 把电脑走的棋标记出来
+    if (isMate(&pos)) {
+        MessageBoxW(NULL, L"祝贺你取得胜利！", L"厉害哇", MB_OKCANCEL);
+    }
+}
+
+void startup(positionStruct* pos) {  // 初始化棋盘
+    pos->blackPlayer = false;
+    pos->vlRed = pos->vlBlack = 0;
+    memcpy(pos->curboard, boardStartup, 256);
+}
+
+
 
 // 点击格子事件处理
 void click(int id) {
@@ -767,12 +1040,13 @@ void click(int id) {
         // 如果点击的不是自己的子，但有子选中了(一定是自己的子)，那么走这个子
         mv = MOVE(idSelected, id);
         if (legalMove(&pos, mv)) {
-            if (makeMove(&pos, mv)) {
+            if (makeMove(&pos, mv, &type, true)) {
                 idSelected = 0;
                 if (isMate(&pos)) {
                     // 如果分出胜负，那么播放胜负的声音，并且弹出不带声音的提示框
                     MessageBoxW(NULL, L"祝贺你取得胜利！", L"厉害哇", MB_OKCANCEL);
                 }
+                
             }
             else {
                 MessageBoxW(NULL, L"将军！", L"警告", MB_OKCANCEL);
@@ -952,7 +1226,8 @@ void processInput() {
 
 
 void update() {
-
+    if (!pos.blackPlayer) return;
+    responseMove(); // 轮到电脑走棋 
 }
 
 int main() {
@@ -962,6 +1237,7 @@ int main() {
         LOG("\n\nrender...\n");
         render();
         processInput();
+        render();
         update();     // 人机对战，电脑做出反应
     }
     closegraph();
